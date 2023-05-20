862884d866ad6e509e080d32590382e5990c1965
Jeni B <78270336+suzyq2nice@users.noreply.github.com>
2023-05-20 10:31:20 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -22,11 +22,10 @@ const Budget = () => {
 	
 	return (
 		<div className='alert alert-secondary'>
-            <span>Budget: {currency}</span>
+            <span>Budget:{currency}</span>
 			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
 		</div>
 	);
 };
 
 export default Budget;
-
